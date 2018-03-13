c17bb6dcc04406db464f1d44793e6248790bf45f
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-03-13 22:05:55 +0800

    Set up a responsive toolbar and theme

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="sideNav" fixed>
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.content">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{ item.content }}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar class="primary">
+      <v-toolbar-side-icon
+        class="hidden-sm-and-up"
+        @click="sideNav = !sideNav">
+      </v-toolbar-side-icon>
+      <v-toolbar-title>FoodFinder</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItems" :key="item.content">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.content }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <!-- Dynamic content goes here -->
+    <main>
+      Content
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        {
+          icon: 'search',
+          content: 'Nearby'
+        },
+        {
+          icon: 'person',
+          content: 'Profile'
+        },
+        {
+          icon: 'face',
+          content: 'Sign Up'
+        },
+        {
+          icon: 'lock_open',
+          content: 'Sign In'
+        }
+      ]
+    }
+  }
+}
+</script>
